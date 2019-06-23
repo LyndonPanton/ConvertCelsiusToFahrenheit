3562dd6334661f59f4082a23681a50f60c244db4
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-23 13:49:29 +0100

    Created function for convert celsius to fahrenheit

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@
 
 window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	function convert(celsius) {
+		return (celsius * (5 / 9)) + 32;
+	}
 };
